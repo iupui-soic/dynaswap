09f55ab860dd7defe94fca6bc52d1c39b2a7a1e2
ChunjieShawn <30278577+ChunjieShawn@users.noreply.github.com>
2019-06-24 15:10:21 -0400

    Updated hierary file

--- dynaswap-omrs/omod/src/main/webapp/DynaSwapApp/services/hierarchy.py
+++ dynaswap-omrs/omod/src/main/webapp/DynaSwapApp/services/hierarchy.py
@@ -56,31 +56,120 @@ class HierarchyGraph:
         self.nodes[parentRoleName].edges[childRoleName] = newEdge
 
     #add a new role
-    def addRole(self, roleName, roleDesc, rolePublicID, privateKey, secondKey):
+    def addRole(self, roleName, roleDesc):
+        pass
 
 
     #read data from database and add roles and edges
     def createGraph(self):
-
+        for roles in Roles.objects.all():
+            self.nodes[roles.role] = Node(roles.role, roles.description, roles.uuid, roles.role_key, roles.role_second_key)
+        for edges in RoleEdges.objects.all():
+            self.nodes[edges.parent_role].edges[edges.child_role] = Edge(edge_key)
 
     #DFS from the specific role
     def isCyclicUtil(self, roleID, visited, recStack):
-
+        # Mark current node as visited and  
+        # adds to recursion stack 
+        visited[roleID] = True
+        recStack[roleID] = True
+  
+        # Recur for all neighbours 
+        # if any neighbour is visited and in  
+        # recStack then graph is cyclic 
+        for neighbour in self.nodes[roleID].edges.keys(): 
+            if !visited[neighbour]: 
+                if self.isCyclicUtil(neighbour, visited, recStack): 
+                    return True
+            elif recStack[neighbour]: 
+                return True
+  
+        # The node needs to be poped from  
+        # recursion stack before function ends 
+        recStack[roleID] = False
+        return False
 
     # Returns true if graph is cyclic else false
     def isCyclic(self):
+        visited = dict() 
+        recStack = dict() 
+        for roleID in self.nodes.keys():
+            visited[roleID] = False
+        recStack = visited.copy()
+        for roleID in self.nodes.keys(): 
+            if !visited[roleID]: 
+                if self.isCyclicUtil(roleID,visited,recStack): 
+                    return True
+        return False
+    
+
+    #update the public ID and access key of the role
+    def updatePublicID(self, roleID):
+        self.nodes[roleID].rolePublicID = hashlib.md5(time.time()).hexdigest()
+        self.nodes[roleID].secondKey = hashlib.md5(self.nodes[roleID].rolePublicID ^ self.nodes[roleID].privateKey)
 
 
     def delEdge(self, parentRoleID, childRoleID):
         #generate a new ID for parent and compute new k
+        self.nodes[parentRoleID].edges.pop(childRoleID)
+        for roles in self.findDesc(childRoleID).keys():
+            self.updatePublicID(roles)
+            Roles.objects.filter(role=roles).update(uuid=self.nodes[roles].rolePublicID, role_second_key=self.nodes[roles].secondKey)
+            for rolePred in self.findPred(roles).keys():
+                self.nodes[rolePred].edges[roles].edgeKey = 
+                RoleEdges.objects.filter().update(edge_key) = self.nodes[rolePred].edges[roles].edgeKey
         #delete record in database
-        #RoleEdge.objects.filter(parent_role=u, child_role=v).delete()
+        RoleEdge.objects.filter(parent_role=parentRoleID, child_role=childRoleID).delete()
 
     
     def delRole(self, RoleID):
         #we may want another dict to store the parents of each role to make del easier
+        for (roleID, roleObj) in self.nodes.items():
+            if roleID == RoleID:
+                #del all children edges
+                for childrenRole in roleObj.edges.keys():
+                    self.delEdge(roleID, childrenRole)
+            if roleObj.edges.has_key(RoleID):
+                #del all parent edges
+                self.delEdge(roleID, RoleID)
+        self.nodes.pop(RoleID)
+        Roles.objects.filter(role=RoleID).delete()
 
     def accessRole(self, curRoleID, targetRoleID):
         #DFS to the role wanted
         #either find the role first then compare keys along the path, or compare keys in DFS
+        if curRoleID == targetRoleID:
+            return True
+        for children in self.nodes[curRoleID].edges.keys():
+            if self.nodes[curRoleID].edges[children].edgeKey ^ Hash(self.nodes[curRoleID].secondKey, self.nodes[children].rolePublicID) == self.nodes[children].secondKey:
+                if self.accessRole(children, targetRoleID):
+                    return True
+        return False
+        
+    #determine if there is a path from origin to the target role
+    def havePath(self, originRoleID, targetRoleID):
+        if originRoleID == targetRoleID:
+            return True
+        for children in self.nodes[originRoleID].edges.keys():
+            if self.havePath(children, targetRoleID):
+                return True
+        return False
+
+    
+    #return the list of descendants of the role
+    def findDesc(self, originRoleID):
+        desc = dict()
+        for roles in self.nodes.keys():
+            if (!desc.has_key(roles) && self.havePath(originRoleID, roles)):
+                desc[roles] = False
+        return desc
+
+    
+    #return the list of immediate predecessors of the role
+    def findPred(self, originRoleID):
+        pred = dict()
+        for roles in self.nodes.keys():
+            if (!pred.has_key(roles) && self.nodes[roles].edges.has_key(originRoleID)):
+                pred[roles] = False
+        return pred
         
