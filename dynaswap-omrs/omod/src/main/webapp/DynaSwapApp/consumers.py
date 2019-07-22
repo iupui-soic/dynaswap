9f10a4e6cddee7218c1358b67f063afe03740692
haakensonb <untamedhawk44@gmail.com>
2019-07-22 11:28:10 -0400

    Cleaned up server_test template

--- dynaswap-omrs/omod/src/main/webapp/DynaSwapApp/consumers.py
+++ dynaswap-omrs/omod/src/main/webapp/DynaSwapApp/consumers.py
@@ -18,16 +18,23 @@ class ServerConsumer(WebsocketConsumer):
         #     'message': message
         # }))
 
-        if action == "request_z":
-            self.publicize_z()
+        if action == "request_public_data":
+            self.publicize_data()
         elif action == "request_SID":
             self.sendSID()
     
-    def publicize_z(self):
-        z = "77777"
+    def publicize_data(self):
+        # These are dummy variables hardcoded in. It is assumed that the ACP class will be able to supply
+        # these either by returning through function calls or passing in to this function as parameters
+        z = 5
+        coefficientList = json.dumps([1, -5, 2])
+        p = 13
         self.send(text_data=json.dumps({
-            'action': 'receive_z',
-            'data': z
+            'action': 'receive_public_data',
+            'z': z,
+            'coefficientList': coefficientList,
+            'p': p
+
         }))
     
     def sendSID(self):
@@ -35,7 +42,7 @@ class ServerConsumer(WebsocketConsumer):
         then this function would take a user_id as an input and be able to detemine if this user already
         has an SID or needs to be assigned a new one."""
         # dummy SID for now, ideally this should use the KeyManagement class from hierarchy.py to get SID
-        SID = "secretID123"
+        SID = 7
         self.send(text_data=json.dumps({
             'action': 'receive_SID',
             'SID': SID
